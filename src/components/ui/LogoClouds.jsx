71a91f53656db3c263e4648a45fc0f802771b6dc
eclipzhl0rd <kuulersog@gmail.com>
2023-09-02 18:54:57 +0100

    UI Changes v2

--- src/components/ui/LogoClouds.jsx
+++ src/components/ui/LogoClouds.jsx
@@ -4,8 +4,8 @@ export default function LogoClouds() {
   return (
     <section className=" md:py-12 overflow-hidden">
       <div className="container  mx-auto">
-        <div className="flex flex-wrap items-center justify-center ">
-          <div className="w-auto pt-6 pb-2">
+        <div className="flex flex-wrap items-center justify-center gap-10 ">
+          <div className="">
             <img
               src="https://seeklogo.com/images/N/next-js-logo-7929BCD36F-seeklogo.com.png"
               alt=""
@@ -13,7 +13,7 @@ export default function LogoClouds() {
               className="invert"
             />
           </div>
-          <div className="w-auto p-6">
+          <div className="">
             <img
               src="https://docs.polyscale.ai/assets/images/supabase-logo-wordmark--light-b617e32c858ef00be44f4e539edb774e.png"
               alt=""
@@ -21,9 +21,17 @@ export default function LogoClouds() {
               className="invert"
             />
           </div>
-          <div className="w-auto p-6">
+          <div className="">
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Tailwind_CSS_logo.svg/2560px-Tailwind_CSS_logo.svg.png"
+              alt=""
+              width={100}
+              className="invert"
+            />
+          </div>
+          <div className="">
+            <img
+              src="https://clerk.com/_next/image?url=%2Fimages%2Fclerk-logo.svg&w=96&q=75"
               alt=""
               width={100}
               className="invert"
